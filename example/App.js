d9d0ca46e3c56f40bbe1e5fdcc0cddb126710cb9
dmacan <david.macan@zeromolecule.com>
2018-02-20 15:37:06 +0100

    Configured example project

--- example/App.js
+++ example/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-zero-ui';
 
 export default class App extends React.Component {
   render() {
