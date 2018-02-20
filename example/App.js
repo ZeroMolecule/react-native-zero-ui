de340f567b1429b4c40d60c20119189b8a92e51e
dmacan <david.macan@zeromolecule.com>
2018-02-20 16:34:03 +0100

    Updated App.js

--- example/App.js
+++ example/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { Button, Input, Text, View } from 'react-native-zero-ui';
 
+const styles = {
+  spacingVertical: {
+    marginVertical: 18
+  },
+  spacingVerticalBig: {
+    marginVertical: 32,
+  }
+};
 export default class App extends React.Component {
   render() {
     return (
       <View styleName="flex center padding-horizontal-l padding-vertical">
-        <Text styleName="h5 center">Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Input placeholder="popuši ga"/>
-        <Button title="E ne budem!"/>
+        <Text style={styles.spacingVertical} styleName="h5 center">Open up App.js to start working
+          on your app!</Text>
+        <Text styleName="center" style={styles.spacingVertical}>Changes you make will automatically
+          reload.</Text>
+        <Text styleName="center" style={styles.spacingVertical}>Shake your phone to open the
+          developer menu.</Text>
+        <Input styleName="center" style={styles.spacingVerticalBig}
+               placeholder="Write something here"/>
+        <Button styleName="round fill " title="E ne budem!"/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
